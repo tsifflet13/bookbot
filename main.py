3cdb0700a1fb672e5f27433a0c4e56fbba9c1b62
Hikiyo <thomas.sifflet@gmail.com>
2025-12-24 16:52:45 +0100

    B: Format report and print

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words_in_text, count_characters_in_text
+from stats import count_words_in_text, count_characters_in_text, sorted_list_of_dicts
 
 def get_book_text(path):
     with open(path) as f:
@@ -8,8 +8,15 @@ def get_book_text(path):
 
 def main():
     text = get_book_text("./books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(count_words_in_text(text))
+    print("--------- Character Count -------")
     char_count_dict = count_characters_in_text(text)
-    print(char_count_dict)
+    sorted_chars = sorted_list_of_dicts(char_count_dict)
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,18 @@
 def sort_on(items):
     return items["num"]
 
+def sorted_list_of_dicts(dict_arg):
+    sorted_list = []
+    for key, value in dict_arg.items():
+        little_dict = {}
+        if key.isalpha() == False:
+            continue
+        else:            
+            little_dict["char"] = key
+            little_dict["num"] = value
+            sorted_list.append(little_dict)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 def count_words_in_text(text_string):
     words = text_string.split()
